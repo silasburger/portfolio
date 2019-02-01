3ec22e3854995485233c135dd867e45374501cb7
Silas Burger <ybsilas@gmail.com>
2019-01-31 17:58:25 -0800

    enabling arrow functions

--- js/index.js
+++ js/index.js
@@ -3,11 +3,4 @@ window.onload = function() {
   let body = document.body;
   overlay.classList.toggle('hidden');
   body.classList.toggle('no-scroll');
-
-  window.addEventListener("keydown", function(e) {
-    // space and arrow keys
-    if([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-        e.preventDefault();
-    }
-}, false);
 };
