d9bca9a1de1268b7bb5311f4daf8d6894c583c95
Silas Burger <ybsilas@gmail.com>
2019-01-31 17:45:39 -0800

    disables scroll keys

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 window.onload = function() {
-  console.log('okoko');
   let overlay = document.querySelector('#overlay');
   let body = document.body;
   overlay.classList.toggle('hidden');
   body.classList.toggle('no-scroll');
+
+  window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
 };
